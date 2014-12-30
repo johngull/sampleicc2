6e446ae80e6d44c29858807427d5278bb1c07c8d
Vitaly Bondar <vitaly@qubyx.com>
2014-12-30 16:23:02 +0200

    copy assignment to itself fix

--- IccProfLib/IccTagDict.cpp
+++ IccProfLib/IccTagDict.cpp
@@ -146,6 +146,9 @@ CIccDictEntry::CIccDictEntry(const CIccDictEntry& IDE)
 ******************************************************************************/
 CIccDictEntry &CIccDictEntry::operator=(const CIccDictEntry &IDE)
 {
+  if(&IDE == this)
+    return *this;
+
   if (m_pNameLocalized)
     delete m_pNameLocalized;
 
